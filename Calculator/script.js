d6738875c5b06180a986adff8ed231dbbb64a59c
Chumik sg <113206669+Chumiksangma@users.noreply.github.com>
2023-12-20 20:26:20 +0530

    Update script.js

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,39 +1,43 @@
-let screen = document.getElementById("screen");
-buttons = document.querySelectorAll("button");
-let screenValue = "";
-for (item of buttons) {
-  item.addEventListener("click", (e) => {
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
 
-    const audio = new Audio();
-    audio.src = "./timer_beep.mp3";
-    audio.play();
-    buttonText = e.target.innerText;
-    console.log("Button text is ", buttonText);
-    if (buttonText == "X") {
-      buttonText = "*";
-      screenValue += buttonText;
-      screen.value = screenValue;
-    } else if (buttonText == "C") {
-      screenValue = "";
-      screen.value = screenValue;
-    } else if (buttonText == "=") {
-      screen.value = eval(screenValue);
-    } else if(buttonText=="Sin"){
-      screen.value = Math.sin(screenValue)
-    }
-    else if(buttonText=="Cos"){
-      screen.value = Math.cos(screenValue)
-    }
-    else if(buttonText=="Tan"){
-      screen.value = Math.tan(screenValue)
-    }
-    else if(buttonText=="Pi"){
-      screen.value = Math.PI
-    }
-    else {
-      screenValue += buttonText;
-      screen.value = screenValue;
-    }
-  });
+<head>
+  <meta charset="utf-8">
+  <title>Simon</title>
+  <link rel="stylesheet" href="styles.css">
+  <link href="https://fonts.googleapis.com/css?family=Press+Start+2P" rel="stylesheet">
+</head>
 
-}
+<body>
+  <h1 id="level-title">Press A Key to Start</h1>
+  <div class="container">
+    <div lass="row">
+
+      <div type="button" id="green" class="btn green">
+
+      </div>
+
+      <div type="button" id="red" class="btn red">
+
+      </div>
+    </div>
+
+    <div class="row">
+
+      <div type="button" id="yellow" class="btn yellow">
+
+      </div>
+      <div type="button" id="blue" class="btn blue">
+
+      </div>
+
+    </div>
+
+  </div>
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="game.js" charset="utf-8"></script>
+
+</body>
+
+</html>
